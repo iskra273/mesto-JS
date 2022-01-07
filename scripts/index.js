952dcd020e9b7be35322b9c5e84d9e958a70b749
Irina Kraskova <iskra273@yandex.ru>
2022-01-07 15:52:03 +0500

    Mesto one

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,28 @@ const popup = document.querySelector('.popup');
 
 function openPopup() {
     popup.classList.add('popup_opened');
+
+    let formElement = document.querySelector('.popup__form');  
+    let nameInput = document.querySelector('.popup__text-title');
+    let jobInput = document.querySelector('.popup__text-subtitle');
+ 
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+   
+    let nameValue = nameInput.value
+    let jobValue = jobInput.value
+   
+    let formNameInput = document.querySelector('.popup__text-title');
+    let formJobInput = document.querySelector('.popup__text-subtitle');
+   
+    function textInput() {
+        nameValue.textContent = formNameInput.value;
+        jobValue.textContent = formJobInput.value;
+    }
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
+
 }
 
 function closePopup() {
@@ -15,23 +37,3 @@ function closePopup() {
 popupOpenButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
 
-let formElement = document.querySelector('.popup__form');   
-let nameInput = document.querySelector('.popup__text-title'); 
-let jobInput = document.querySelector('.popup__text-subtitle');
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    
-    nameInput.value
-    jobInput.value
-    
-    let formNameInput = nameInput.value
-    let formJobInput = jobInput.value
-    
-    function textInput() {
-        nameInput.textContent = formNameInput.value; 
-        jobInput.textContent = formJobInput.value;
-    }
-}
-
-formElement.addEventListener('submit', formSubmitHandler); 
