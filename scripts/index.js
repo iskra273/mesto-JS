80b01317f4cef4df439ee3346e126484985ad5b0
Irina Kraskova <iskra273@yandex.ru>
2022-01-14 20:05:28 +0500

    Mesto version pravka

--- scripts/index.js
+++ scripts/index.js
@@ -3,28 +3,29 @@ const popupCloseButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
 
 let formElement = document.querySelector('.popup__form');  
-let nameInput = document.querySelector('.popup__input popup__input_name_title');
-let jobInput = document.querySelector('.popup__input popup__input_name_subtitle');
+let nameInput = document.querySelector('.popup__input_name_title');
+let jobInput = document.querySelector('.popup__input_name_subtitle');
 let profileNameInput = document.querySelector('.profile__info-title');
 let profileJobInput = document.querySelector('.profile__info-subtitle');
 
 
 function openPopup() {
     popup.classList.add('popup_opened');
+    addProfileInput();
 }
 
 function addProfileInput() {
-    profileNameInput.textContent = nameInput.value;
-    profileJobInput.textContent = jobInput.value;
+    nameInput.value = profileNameInput.textContent; 
+    jobInput.value = profileJobInput.textContent;
 }
 
 
 function handleProfileEditSubmit (evt) {
     evt.preventDefault();          
     profileNameInput.textContent = nameInput.value;
-    profileJobInput.textContent = jobInput.value; 
-    nameInput.textContent = profileNameInput;
-    jobInput.textContent = profileJobInput;  
+    profileJobInput.textContent = jobInput.value;
+    
+    closePopup();
 }
 
 
@@ -33,6 +34,5 @@ function closePopup() {
 }
 
 popupOpenButton.addEventListener('click', openPopup);
-popupOpenButton.addEventListener('click', addProfileInput);
 formElement.addEventListener('submit', handleProfileEditSubmit);
 popupCloseButton.addEventListener('click', closePopup);
