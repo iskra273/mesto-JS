4f522a58459ff639d048b5bdf4f2902a2216ad5c
Irina Kraskova <iskra273@yandex.ru>
2022-01-24 17:02:50 +0500

    Mesto second pravka

--- scripts/index.js
+++ scripts/index.js
@@ -23,28 +23,29 @@ const initialCards = [
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
-];    
-
+];
 
 // Модальные окна
-const editModal =  document.querySelector('.popup_type_edit')
-const addCardModal = document.querySelector('.popup_type_add-element')
-const openImageCardModal = document.querySelector('.popup_type_image-element') 
+const profileEditModal =  document.querySelector('.popup_type_edit')
+const cardAddModal = document.querySelector('.popup_type_add-element')
+const imageOpenModal= document.querySelector('.popup_type_image-element') 
 
 // Поля профиля
-
 const profileTitle =  document.querySelector('.profile__info-title')
 const profileSubtitle = document.querySelector('.profile__info-subtitle')
 
 // Формы
-
-const editForm =  editModal.querySelector('.popup__form')
-const addCardForm =  addCardModal.querySelector('.popup__form')
+const profileEditForm =  profileEditModal.querySelector('.popup__form')
+const cardAddForm =  cardAddModal.querySelector('.popup__form')
 
 // Кнопки
+const profileEditButton = document.querySelector('.profile__edit');
+const profileEditModalCloseButton = profileEditModal.querySelector('.popup__close');
 
-const editProfileButton = document.querySelector('.profile__edit');
-const closeEditModalButton = editModal.querySelector('.popup__close');
+const cardAddButton = document.querySelector('.profile__add')
+const cardAddModalCloseButton = cardAddModal.querySelector('.popup__close');
+
+const imageCloseButton = imageOpenModal.querySelector('.popup__close');
 
 // Инпуты
 const inputCardName = document.querySelector('.popup__input_name_add-name')
@@ -54,70 +55,65 @@ const inputProfileTitle = document.querySelector('.popup__input_name_title')
 const inputProfileSubtitle = document.querySelector('.popup__input_name_subtitle')
 
 // 
-const addCardButton = document.querySelector('.profile__add')
-const closeAddCardModalButton = addCardModal.querySelector('.popup__close'); 
-
 const list = document.querySelector('.elements')  
 const elementTemplate = document.querySelector('.element-template').content
 
 
-
 function toggleModal(modal) {
   modal.classList.toggle('popup_opened');
-  addProfileInput();
 }
 
-function addProfileInput() {
+
+profileEditButton.addEventListener('click', () => {
+  toggleModal(profileEditModal)
   inputProfileTitle.value = profileTitle.textContent; 
   inputProfileSubtitle.value = profileSubtitle.textContent;
-}
-
-editProfileButton.addEventListener('click', () => toggleModal(editModal))
-closeEditModalButton.addEventListener('click', () => toggleModal(editModal))
+} )
+profileEditModalCloseButton .addEventListener('click', () => toggleModal(profileEditModal))
   
 
-addCardButton.addEventListener('click', () => toggleModal(addCardModal))
-closeAddCardModalButton.addEventListener('click', () => toggleModal(addCardModal))
+cardAddButton.addEventListener('click', () => toggleModal(cardAddModal))
+cardAddModalCloseButton.addEventListener('click', () => toggleModal(cardAddModal))
   
 
-addCardForm.addEventListener('submit', (event) => {
+imageCloseButton.addEventListener('click', () => toggleModal(imageOpenModal));
+
+cardAddForm.addEventListener('submit', (event) => {
   event.preventDefault()
   createElement({
     name: inputCardName.value,
     link: inputCardLink.value
   })
 
-  toggleModal(addCardModal)
+  toggleModal(cardAddModal)
 })
 
 
-editForm.addEventListener('submit', (event) => {
+profileEditForm.addEventListener('submit', (event) => {
   event.preventDefault()
   
   profileTitle.textContent = inputProfileTitle.value
   profileSubtitle.textContent = inputProfileSubtitle.value
 
-  toggleModal(editModal)
+  toggleModal(profileEditModal)
 
 })
 
 
-function createElement(elementData) {
+function createCard (item) {
   const cardElement = elementTemplate.cloneNode(true)
   const elementImage = cardElement.querySelector('.element__image')
   const elementName = cardElement.querySelector('.element__name')
   const deleteButton = cardElement.querySelector('.element__delete')
   const likeButton = cardElement.querySelector('.element__like')
   const card = cardElement.querySelector('.element')
-  const popupImage =  document.querySelector('.popup_type_image-element')
-  const popupCloseImage = document.querySelector('.popup__close_image_vert')
-  const popupImagePhoto= document.querySelector('.popup__image-element')
+  const popupImagePhoto =  document.querySelector('.popup__image-element')
   const popupImageCaption =  document.querySelector('.popup__caption')
   
   
-  elementName.textContent = elementData.name
-  elementImage.src = elementData.link
-  elementImage.alt = elementData.name
+  elementName.textContent = item.name
+  elementImage.src = item.link
+  elementImage.alt = item.name
 
   
   function deleteElement() {
@@ -125,32 +121,27 @@ function createElement(elementData) {
   }
 
   function addLike() {
-    likeButton.classList.add('element__like_active');
+    likeButton.classList.toggle('element__like_active');
   }
- 
-  function openPopupImage() {
-    popupImage.classList.add('popup_opened');
-    addImageElement()
-  }
-
-  function addImageElement() {
-    popupImagePhoto.src = elementData.link
-    popupImageCaption.textContent = elementData.name
-  }
-
-  function closePopupImage() {
-    popupImage.classList.remove('popup_opened');
-}
   
-
   likeButton.addEventListener('click', addLike)
   deleteButton.addEventListener('click', deleteElement)
-  elementImage.addEventListener('click', openPopupImage)
-  popupCloseImage.addEventListener('click', closePopupImage)
-  
 
-  list.prepend(cardElement)
+  elementImage.addEventListener('click', () => {
+    toggleModal(imageOpenModal)
+
+    popupImagePhoto.src = item.link
+    popupImageCaption.textContent = item.name
+  });
+  
+  return cardElement;
   
 }
+
+function createElement(elementData) {
+  const cardElement = createCard(elementData)
+  list.prepend(cardElement)
+}
+
 
 initialCards.forEach(createElement)
